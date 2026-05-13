e051c2462a2697dd62d0abc91af549ba20950a08
mcrco <marco.yang.ca@gmail.com>
2026-05-12 23:31:43 -0700

    add rope for qwen2 layer + annotate

--- hw5/transformer-main/src/qwen2/Qwen2Layer.cu
+++ hw5/transformer-main/src/qwen2/Qwen2Layer.cu
@@ -23,15 +23,15 @@ __device__ inline float normalize_float(__nv_bfloat16 x) {
     return __bfloat162float(x);
 }
 
-template<typename residual_float_t>
-__global__ void residualAddKernel(__nv_bfloat16 *hidden_state, residual_float_t *residual, int n) {
+template<typename value_t>
+__global__ void residualAddKernel(__nv_bfloat16 *residual, value_t *values, int n) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int stride = blockDim.x * gridDim.x;
 
     for (int i = idx; i < n; i += stride) {
-        float hval = __bfloat162float(hidden_state[i]);
         float rval = normalize_float(residual[i]);
-        hidden_state[idx] = __float2bfloat16(hval + rval);
+        float vval = __bfloat162float(values[i]);
+        residual[idx] = __float2bfloat16(rval + vval);
     }
 }
 
@@ -47,46 +47,60 @@ Qwen2Layer<QWEN2_SIZE>::Qwen2Layer(uint32_t layer_num, uint32_t max_seq_len):
 
 template<Qwen2Size QWEN2_SIZE>
 void Qwen2Layer<QWEN2_SIZE>::forward(const std::shared_ptr<CudaBuffer>& k_cache, const std::shared_ptr<CudaBuffer> &v_cache, const std::shared_ptr<CudaBuffer> &hidden_state, int32_t seq_len, cudaStream_t stream) {
+    // First layernorm
+    input_layernorm.normalize_hidden_state(hidden_state, hidden_state, stream);
+
+    // QKV projections.
+    // First get pointers from buffers.
     __nv_bfloat16 *q_proj_weight_ptr = static_cast<__nv_bfloat16 *>(q_proj_weight->data);
     __nv_bfloat16 *q_proj_bias_ptr = static_cast<__nv_bfloat16 *>(q_proj_bias->data);
     __nv_bfloat16 *k_proj_weight_ptr = static_cast<__nv_bfloat16 *>(k_proj_weight->data);
     __nv_bfloat16 *k_proj_bias_ptr = static_cast<__nv_bfloat16 *>(k_proj_bias->data);
     __nv_bfloat16 *v_proj_weight_ptr = static_cast<__nv_bfloat16 *>(v_proj_weight->data);
     __nv_bfloat16 *v_proj_bias_ptr = static_cast<__nv_bfloat16 *>(v_proj_bias->data);
-    __nv_bfloat16 *gate_proj_weight_ptr = static_cast<__nv_bfloat16 *>(gate_proj_weight->data);
-    __nv_bfloat16 *up_proj_weight_ptr = static_cast<__nv_bfloat16 *>(up_proj_weight->data);
-    __nv_bfloat16 *down_proj_weight_ptr = static_cast<__nv_bfloat16 *>(down_proj_weight->data);
-
     __nv_bfloat16 *k_cache_ptr = static_cast<__nv_bfloat16 *>(k_cache->data);
     __nv_bfloat16 *v_cache_ptr = static_cast<__nv_bfloat16 *>(v_cache->data);
     __nv_bfloat16 *hidden_state_ptr = static_cast<__nv_bfloat16 *>(hidden_state->data);
     __nv_bfloat16 *queries_ptr = static_cast<__nv_bfloat16 *>(attention_output->data);
-
-    float *attention_output_ptr = static_cast<float *>(attention_output->data);
-    __nv_bfloat16 *gate_proj_ptr = static_cast<__nv_bfloat16 *>(gate_proj->data);
-    __nv_bfloat16 *up_proj_ptr = static_cast<__nv_bfloat16 *>(up_proj->data);
-    __nv_bfloat16 *down_proj_ptr = static_cast<__nv_bfloat16 *>(down_proj->data);
-
-    input_layernorm.normalize_hidden_state(hidden_state, hidden_state, stream);
-
+    // Query projection + RoPE (store inside state).
     MatrixVectorMultiply::bf16_matmul(Qwen2Config::hidden_size(), Qwen2Config::hidden_size(), q_proj_weight_ptr, q_proj_bias_ptr, hidden_state_ptr, queries_ptr, stream);
+    RoPE::apply_rope_to_qk(queries_ptr, Qwen2Config::num_query_heads(), Qwen2Config::head_size(), seq_len, Qwen2Config::rope_theta_base(), stream);
+    // Compute where current token's KV vectors should be stored in KV cache.
     __nv_bfloat16 *key_ptr = k_cache_ptr + seq_len * Qwen2Config::num_layers() * Qwen2Config::keys_size();
     __nv_bfloat16 *val_ptr = v_cache_ptr + seq_len * Qwen2Config::num_layers() * Qwen2Config::values_size();
+    // Key and value projections (store inside cache).
     MatrixVectorMultiply::bf16_matmul(Qwen2Config::hidden_size(), Qwen2Config::hidden_size(), k_proj_weight_ptr, k_proj_bias_ptr, hidden_state_ptr, key_ptr, stream);
     MatrixVectorMultiply::bf16_matmul(Qwen2Config::hidden_size(), Qwen2Config::hidden_size(), v_proj_weight_ptr, v_proj_bias_ptr, hidden_state_ptr, val_ptr, stream);
+    // RoPE for keys.
+    RoPE::apply_rope_to_qk(key_ptr, Qwen2Config::num_kv_heads(), Qwen2Config::head_size(), seq_len, Qwen2Config::rope_theta_base(), stream);
 
+    // Self-attention.
+    // Get pointer from cuda buffer.
+    float *attention_output_ptr = static_cast<float *>(attention_output->data);
+    // Store attention inside state.
     GroupQueryAttention<QWEN2_SIZE>::sdpa(queries_ptr, k_cache_ptr, v_cache_ptr, attention_output_ptr, layer_num, seq_len, stream);
     int threads = RES_ADD_THREADS;
     int blocks = min(RES_ADD_MAX_BLOCKS, ((int)Qwen2Config::hidden_size() + threads - 1) / threads);
     residualAddKernel<float><<<blocks, threads, 0, stream>>>(hidden_state_ptr, attention_output_ptr, Qwen2Config::hidden_size());
     checkCuda(cudaGetLastError());
 
+    // Layernorm after attention + residual connection.
     post_attention_layernorm.normalize_hidden_state(hidden_state, hidden_state, stream);
 
+    // Final MLP.
+    // Get pointers from buffers.
+    __nv_bfloat16 *gate_proj_weight_ptr = static_cast<__nv_bfloat16 *>(gate_proj_weight->data);
+    __nv_bfloat16 *up_proj_weight_ptr = static_cast<__nv_bfloat16 *>(up_proj_weight->data);
+    __nv_bfloat16 *down_proj_weight_ptr = static_cast<__nv_bfloat16 *>(down_proj_weight->data);
+    __nv_bfloat16 *gate_proj_ptr = static_cast<__nv_bfloat16 *>(gate_proj->data);
+    __nv_bfloat16 *up_proj_ptr = static_cast<__nv_bfloat16 *>(up_proj->data);
+    __nv_bfloat16 *down_proj_ptr = static_cast<__nv_bfloat16 *>(down_proj->data);
+    // Store intermediate results in state.
     MatrixVectorMultiply::bf16_matmul(Qwen2Config::hidden_size(), Qwen2Config::intermediate_size(), gate_proj_weight_ptr, nullptr, hidden_state_ptr, gate_proj_ptr, stream);
     MatrixVectorMultiply::bf16_matmul(Qwen2Config::hidden_size(), Qwen2Config::intermediate_size(), up_proj_weight_ptr, nullptr, hidden_state_ptr, up_proj_ptr, stream);
     SiLUMult::silu_mult_in_place(gate_proj, up_proj, stream);
     MatrixVectorMultiply::bf16_matmul(Qwen2Config::intermediate_size(), Qwen2Config::hidden_size(), down_proj_weight_ptr, nullptr, gate_proj_ptr, down_proj_ptr, stream);
+    // Add back to hidden state via residual.
     residualAddKernel<__nv_bfloat16><<<blocks, threads, 0, stream>>>(hidden_state_ptr, down_proj_ptr, Qwen2Config::hidden_size());
 }
 
